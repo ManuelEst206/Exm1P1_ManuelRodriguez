95a1a7c2f3b52b36d71031ae4c8b2bc8ab3f1561
Manuel Rodríguez <manu_jr@outlook.com>
2026-05-22 13:20:40 -0600

    Commit 1 Examen

---
+++ src/exm1p1_manuelrodriguez/Exm1P1_ManuelRodriguez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exm1p1_manuelrodriguez;
+
+/**
+ *
+ * @author manu_
+ */
+public class Exm1P1_ManuelRodriguez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
