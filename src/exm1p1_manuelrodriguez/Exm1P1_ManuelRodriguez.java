b884ea7195f14a7c8d8b964f882ab75e8ae7c209
Manuel Rodríguez <manu_jr@outlook.com>
2026-05-22 14:21:55 -0600

    Segundo Commit Exm1P1

--- src/exm1p1_manuelrodriguez/Exm1P1_ManuelRodriguez.java
+++ src/exm1p1_manuelrodriguez/Exm1P1_ManuelRodriguez.java
@@ -4,6 +4,8 @@
  */
 package exm1p1_manuelrodriguez;
 
+import java.util.Scanner;
+
 /**
  *
  * @author manu_
@@ -14,7 +16,31 @@ public class Exm1P1_ManuelRodriguez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        
+        do{
+            menu();
+            int opcion = scanner.nextInt();
+            switch(opcion){
+                case 1:
+                    System.out.println("CONVERSION BINARIO A DECIMAL");
+                    break;
+                case 2:
+                    System.out.println("CONTAINS MANUAL");
+                    break;
+                case 3:
+                    System.out.println(" REEMPLAZO DE PALABRAS");
+                    break;
+            }
+        }
+        
     }
-    
+    public static void menu(){
+        System.out.println("------MENU------");
+        System.out.println("1. Conversion de Binario a Decimal");
+        System.out.println("2. Contains Manual");
+        System.out.println("3. Reemplazo de palabras");
+        System.out.println("4. Salir");
+        System.out.print("Ingrese una opcion");
+    }
 }
